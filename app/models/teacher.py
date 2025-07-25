1cf282e81216d384f5d6be04ee11d8dac47685eb
Raymond Degbe <198380402+mrdegbe@users.noreply.github.com>
2025-07-25 01:36:16 +0100

    feat(alembic): Remove homeroom_class_id column from teachers model

--- app/models/teacher.py
+++ app/models/teacher.py
@@ -22,7 +22,6 @@ class Teacher(Base):
     address = Column(String)
     specialization = Column(String, nullable=False)
     user_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"))
-    homeroom_class_id = Column(Integer, ForeignKey("classes.id"))
 
     created_at = Column(
         DateTime(timezone=True), server_default=func.now(), nullable=False
